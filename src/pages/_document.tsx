e0f89eb1fa2563bc0e19ba40e0db84d54fd1aa98
Kaz.N4 <117555054+kztnso@users.noreply.github.com>
2023-05-13 21:31:15 +0900

    Add information of this website

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,16 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ja">
+      <Head>
+        {/* Environment Settings */}
+        <meta charSet='utf-8' />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <link rel="icon" type="image/ico" href="/favicon.ico" />
+
+        {/* Website Information */}
+        <meta name="description" content="学生向けの学習補助サービス" />
+      </Head>
       <body>
         <Main />
         <NextScript />
